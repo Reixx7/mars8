8938e152bd6a7b90c5c0daf0bb7421981c40ea97
Reixx7 <reixx23and7@gmail.com>
2025-09-09 23:08:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,16 +5,18 @@ let btn = document.querySelector('.pomidor');
 
 btn.addEventListener('click', (event)=>{
     event.preventDefault()
-    if (input1.value == "23072307" && input2.value == "12345") {
+    if (input1.value == localStorage.getItem("login") && input2.value == localStorage.getItem("password")) {
         window.location.href = "about.html";
-    }else(input1.value == "" && input2.value == ""); {
+    }else {
         alert("Такого аккаунта не существует!")
         console.log(input1.value)
        
 }
- localStorage.setItem("login", input1.value);
- localStorage.setItem("password", input2.value);
-    }
+ localStorage.setItem( "login","23072307");
+ localStorage.setItem("password","12345" );
+   
 
+ 
+}
     
 );
